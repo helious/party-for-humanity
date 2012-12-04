ec1bc6559dc8b2e97b3bf1ea7144d0883a55ff69
David Posey <rangemonk@gmail.com>
2012-12-04 18:57:48 -0500

    Initiating rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyForHumanity::Application.config.secret_token = 'fab333feef11b878176712937be461707b4bdc12a3ae25df4aec19671d3208bf662a3f58d97e7d9320147f2755d3375422ce606d627faa7795a0173f7c16f20e'
