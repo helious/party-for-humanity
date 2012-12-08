610385729d54b9b40bb8ae2973850525dc3d4cca
David Posey <rangemonk@gmail.com>
2012-12-08 18:20:19 -0500

    Share/suggest form skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -53,13 +53,14 @@ PartyForHumanity::Application.routes.draw do
   match '/learn' => 'static#learn', :as => :learn
   match '/choose' => 'static#choose', :as => :choose
   match '/organize' => 'static#organize', :as => :organize
-  match '/share' => 'static#share', :as => :share
   match '/who-we-are' => 'static#who_we_are', :as => :who_we_are
   match '/board' => 'static#board', :as => :board
   match '/donate' => 'static#donate', :as => :donate
   match '/directors-letter' => 'static#directors_letter', :as => :directors_letter
   match '/financial-statements' => 'static#financial_statements', :as => :financial_statements
   match '/privacy' => 'static#privacy', :as => :privacy
+  match '/share' => 'static#contact', :as => :share
+  match '/suggest' => 'static#contact', :as => :suggest
 
   # See how all your routes lay out with "rake routes"
 
