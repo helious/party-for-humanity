a942b3de2a615022d6e83e93a6e9626ce92c70ad
David Posey <rangemonk@gmail.com>
2013-02-07 22:40:45 -0500

    Extending classes the correct way.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module PartyForHumanity
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- config/initializers/core_ext.rb
+++
@@ -1,4 +0,0 @@
-require 'CGI'
-require 'core_ext/string'
-require 'core_ext/hash'
-require 'core_ext/nil_class'
