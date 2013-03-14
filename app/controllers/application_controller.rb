80aaa25428796a516aa8b2e5594dcd5402f488de
David Posey <rangemonk@gmail.com>
2013-03-14 02:40:02 -0400

    Adding reference to resources.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :create_profile_reference, :if => Proc.new { user_signed_in? }
+  before_filter :assign_common_globals
   before_filter :clean_params_of_default_rails_form_param_additions, :if => Proc.new { params[:controller] == 'messages' }
 
+  def assign_common_globals
+    @controller = params[:controller]
+  end
+  
   def clean_params_of_default_rails_form_param_additions
   	[:utf8, :authenticity_token, :commit, :controller, :action].each { |v| params.delete v }
 
