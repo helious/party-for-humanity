44017782075751e5872e60a174de51fa3082efd9
David Posey <rangemonk@gmail.com>
2013-04-23 01:44:02 -0400

    Tryign new from emails.

--- app/mailers/invitation.rb
+++ app/mailers/invitation.rb
@@ -14,7 +14,7 @@ class Invitation < ActionMailer::Base
 
 		@charity = Charity.find_by_id party.charity_id
 
-		mail :to => guest.email, :from => "#{party.user.email} <partyforhumanity.org>", :subject => "#{party.name} - You're Invited!" do |format|
+		mail :to => guest.email, :from => "info@partyforhumanity.org", :subject => "#{party.name} - You're Invited!" do |format|
 			format.text
 			format.html
 		end
@@ -22,7 +22,7 @@ class Invitation < ActionMailer::Base
 
 	def to_parent(email)
 		headers "X-SMTPAPI" => category_header(:email_to_parent)
-		mail :to => email, :from => "Registration <registration@partyforhumanity.org>", :subject => "Registration - We Need Parental Confirmation" do |format|
+		mail :to => email, :from => "registration@partyforhumanity.org", :subject => "Registration - We Need Parental Confirmation" do |format|
 			format.text
 			format.html
 		end
