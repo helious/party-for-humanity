bf2647f5535de3c230464bd04ad6ca2f3c553539
David Posey <rangemonk@gmail.com>
2013-04-23 01:59:22 -0400

    more mail stuff.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,5 +43,5 @@ PartyForHumanity::Application.configure do
   }
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'partyforhumanity.org' }
 end
