8709e97a9c21e236b2176c3518149e5e77c41a74
David Posey <rangemonk@gmail.com>
2013-04-29 21:19:23 -0400

    Profile weirddd.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,13 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 
 	validates_uniqueness_of :username
+
+	before_save :nil_out
+
+	private
+	def nil_out
+		self.username = nil if self.username.blank?
+		self.name = nil if self.name.blank?
+		self.zipcode = nil if self.zipcode.blank?
+	end
 end
