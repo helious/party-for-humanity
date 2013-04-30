f76e630c4e60fdd4290a67c6aae6d8b3c66a8b23
David Posey <rangemonk@gmail.com>
2013-04-29 21:29:58 -0400

    This is crazy.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :omniauthable, :omniauth_providers => [:facebook, :twitter]
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :trackable, :validatable
-
+  after_save :create_profile
   has_one :profile, :dependent => :destroy
   has_many :parties
   accepts_nested_attributes_for :profile
@@ -21,6 +21,13 @@ class User < ActiveRecord::Base
       self.where('id = ?', profile.user_id).limit(1).first 
     else
       self.where('email = ?', conditions[:email]).limit(1).first
+    end
+  end
+
+  def create_profile
+    if self.profile.nil?
+      self.profile = Profile.new
+      self.profile.save
     end
   end
 
@@ -45,7 +52,6 @@ class User < ActiveRecord::Base
     unless user
       user = User.create(provider:auth.provider, uid:auth.uid, email:"#{auth.extra.raw_info.screen_name}@twitter.com", password:Devise.friendly_token[0,20])
 
-      user.create_profile
       user.profile.name = auth.extra.raw_info.name
       user.profile.username = auth.extra.raw_info.screen_name
 
