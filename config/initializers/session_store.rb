d2f3d488c37318080c46956f27b7fc9bad8f15f6
David Posey <rangemonk@gmail.com>
2013-05-09 00:13:51 -0400

    Oh yea. migrationa dd that junk.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PartyForHumanity::Application.config.session_store :cookie_store, key: '_party-for-humanity_session'
+#PartyForHumanity::Application.config.session_store :cookie_store, key: '_party-for-humanity_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
