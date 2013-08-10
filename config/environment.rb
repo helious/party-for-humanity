6aff40ee8b917b41f81c691807af285289a05668
David Posey <rangemonk@gmail.com>
2013-08-10 14:44:52 -0400

    Email fix for forgot pass.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PartyForHumanity::Application.initialize!
-
-PartyForHumanity::Application.configure do
-	config.action_mailer.default_url_options = { :host => 'localhost' }
-end
